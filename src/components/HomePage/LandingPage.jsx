6c2afb3d38bfc65f721d9e080a1e035c43cb0913
gabbypile12 <gabbyp.447@gmail.com>
2024-10-26 22:47:02 -0400

    adding styling to the front page

--- src/components/HomePage/LandingPage.jsx
+++ src/components/HomePage/LandingPage.jsx
@@ -4,19 +4,26 @@ import WhiteRabbit from "../../assets/whiteRabbitRemoved.png"
 import QueenOfHeartes from "../../assets/queenOfHeartesRemoved.png"
 import MadHatterRemoved from "../../assets//madHatterRemoved (2).png"
 import Alice from "../../assets/alice.png"
+import React, { useState, useEffect } from 'react'; 
 
 import Table from 'react-bootstrap/Table';
 
 function LandingPage(){
+    const [isHovering, setIsHovering] = useState(false);
+    const [isHovering2, setIsHovering2] = useState(false);
+    const [isHovering3, setIsHovering3] = useState(false);
+    const [isHovering4, setIsHovering4] = useState(false);
+
+
     return(
         <div>
              <h1>Welcome to Alice's First Year Computer Science Study Guide!</h1>
              <Table>
                 <tbody>
                     <tr>
-                        <td><img src = {CheshireCat} className = "landPageImage"/></td>
+                        <td className={`my-div ${isHovering ? 'hovered' : ''}`} onMouseEnter={() => setIsHovering(true)} onMouseLeave={() => setIsHovering(false)}><img src = {CheshireCat} className = "landPageImage"/> <h2> Java </h2></td>
                         <td></td>
-                        <td> <td><img src = {WhiteRabbit} className = "landPageImage"/></td></td>
+                        <td className={`my-div ${isHovering2 ? 'hovered' : ''}`} onMouseEnter={() => setIsHovering2(true)} onMouseLeave={() => setIsHovering2(false)}> <img src = {WhiteRabbit} className = "landPageImage2"/> <h2> Python </h2></td>
                  </tr>
                  <tr>
                     <td></td>
@@ -24,12 +31,12 @@ function LandingPage(){
                     <td></td>
                 </tr>
                 <tr>
-                    <td><img src = {QueenOfHeartes} className = "landPageImage"/></td>
+                    <td className={`my-div ${isHovering3 ? 'hovered' : ''}`} onMouseEnter={() => setIsHovering3(true)} onMouseLeave={() => setIsHovering3(false)}> <img src = {QueenOfHeartes} className = "landPageImage2"/> <h2>Object Oriented programming</h2></td>
                     <td></td>
-                    <td><img src = {MadHatterRemoved} className = "landPageImage"/></td>
+                    <td className={`my-div ${isHovering4 ? 'hovered' : ''}`} onMouseEnter={() => setIsHovering4(true)} onMouseLeave={() => setIsHovering4(false)}> <img src = {MadHatterRemoved} className = "landPageImage3"/> <h2>Study Tips</h2></td>
                 </tr>
             </tbody>
-
+ 
              </Table>
         </div>
     )
